3a10bad6e0258a66eadb51ea3f3e82b59fafcd33
Hugo <yufei.a.chen@rakuten.com>
2024-06-07 10:12:44 +0900

    add: apple fancy formatter

---
+++ src/main/java/lambdasinaction/common/AppleFancyFormatter.java
@@ -0,0 +1,9 @@
+package lambdasinaction.common;
+
+public class AppleFancyFormatter implements AppleFormatter {
+
+  public String accept(Apple apple) {
+    String characteristic = apple.getWeight() > 150 ? "heavy" : "light";
+    return "A " + characteristic + " " + apple.getColor() + " apple";
+  }
+}
