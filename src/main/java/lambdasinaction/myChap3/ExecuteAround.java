eb171daba1aeec5d9665065933ad38a7e3de0f9b
Hugo <yufei.a.chen@rakuten.com>
2024-06-15 17:16:04 +0900

    add: first add

--- src/main/java/lambdasinaction/myChap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/myChap3/ExecuteAround.java
@@ -1,9 +1,25 @@
 package lambdasinaction.myChap3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import lambdasinaction.common.BufferedReaderProcessor;
 
 public class ExecuteAround {
-    public static void main(String[] args) {
 
+  static final String TEST_FILE = "/Users/yufei.a.chen/IdeaProjects/Java8InAction"
+      + "/src/main/resources/lambdasinaction/chap3/data.txt";
+
+  public static void main(String[] args) throws IOException {
+    String oneLine = processFile(BufferedReader::readLine);
+    String twoLine = processFile((BufferedReader br) -> br.readLine() + br.readLine());
+    System.out.println(oneLine);
+    System.out.println(twoLine);
+  }
+
+  public static String processFile(BufferedReaderProcessor p) throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader(TEST_FILE))) {
+      return p.process(br);
     }
+  }
 }
